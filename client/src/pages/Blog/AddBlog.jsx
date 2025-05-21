ca15b88998b61c5482c8fc204a00e5f04d8a4e21
Anupras Sharma <sharmaanupras7@gmail.com>
2025-05-21 23:26:52 +0530

    Update AddBlog.jsx

--- client/src/pages/Blog/AddBlog.jsx
+++ client/src/pages/Blog/AddBlog.jsx
@@ -26,7 +26,7 @@ import { RouteBlog } from '@/helpers/RouteName'
 const AddBlog = () => {
     const navigate = useNavigate()
     const user = useSelector((state) => state.user)
-    const { data: categoryData, loading, error } = useFetch(`${getEvn('VITE_API_BASE_URL')}/category/all-category`, {
+    const { data: categoryData, loading, error } = useFetch(`${getEvn('VITE_API_BASE_URL || https://write-flow-api.onrender.com')}/category/all-category`, {
         method: 'get',
         credentials: 'include'
     })
