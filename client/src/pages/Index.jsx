4c60d2431e6edf74a7e77816a6a611c4bfb2b4ff
Anupras Sharma <sharmaanupras7@gmail.com>
2025-05-21 23:26:03 +0530

    Update Index.jsx

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -5,7 +5,7 @@ import { useFetch } from '@/hooks/useFetch'
 import React from 'react'
 
 const Index = () => {
-    const { data: blogData, loading, error } = useFetch(`${getEvn('VITE_API_BASE_URL')}/blog/blogs`, {
+    const { data: blogData, loading, error } = useFetch(`${getEvn('VITE_API_BASE_URL || https://write-flow-api.onrender.com')}/blog/blogs`, {
         method: 'get',
         credentials: 'include'
     })
